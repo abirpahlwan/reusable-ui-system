63854eee3999b9d4f03d08a732e2c0617c74a844
Pahlwan Rabiul Abir <abir978@gmail.com>
2018-08-18 09:23:16 +0600

    Lecture 010 -> Creating a Menu Setup

---
+++ Assets/Scripts/Editor/UIMenu.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace Reusable.UI {
+	public class UIMenu : MonoBehaviour {
+		
+		[MenuItem("GameObject/UI/Reusable UI/UIGroup")]
+		public static void CreateUIGroup() {
+			GameObject uiGroup = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/Prefabs/UIGroup.prefab");
+
+			if (uiGroup) {
+				GameObject createdGroup = Instantiate(uiGroup);
+				createdGroup.name = "UIGroup";
+			} else {
+				EditorUtility.DisplayDialog("Reusable UI Warning", "Cannot find UIGroup Prefab", "OK");
+			}
+		}
+	}
+}
